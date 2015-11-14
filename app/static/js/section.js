7f5876acd4ff0793a0e096516f7f13d8f3bf1abd
Partin <partin.imeri@gmail.com>
2015-11-14 16:59:36 +0100

    Improved UI of selecting units and projects to compare.

--- app/static/js/section.js
+++ app/static/js/section.js
@@ -1,15 +1,62 @@
 (function() {
-	[].slice.call( document.querySelectorAll( '.stack-project' ) ).forEach( function( el ) {
-		var togglebtt = el.previousElementSibling,
-			togglefn = function() {
-				if( classie.hasClass( el, 'active' ) ) {
-					classie.removeClass( el, 'active' );
+	[].slice.call( document.querySelectorAll( '.project-item' ) ).forEach( function( el ) {
+		var stack_grid = $('.stack-project');
+		el.addEventListener( 'click', function() {
+				if( classie.hasClass( stack_grid[0], 'active' ) ) {
+					classie.removeClass( stack_grid[0], 'active' );
+					var clicked_image_id = this.id;
+
+					if(clicked_image_id == 1){
+						$(".pitem1").css('transform', 'scale(1)');
+						$(".pitem2").css('transform', 'scale(0)');
+						$(".pitem3").css('transform', 'scale(0)');
+						$(".pitem4").css('transform', 'scale(0)');
+						$(".unit-item img:nth-child(1)").css('transform-origin','100% 100%');
+						$(".unit-item img:nth-child(2)").css('transform-origin','0 100%');
+						$(".unit-item img:nth-child(3)").css('transform-origin','100% 0%');
+						$(".unit-item img:nth-child(4)").css('transform-origin','0 0');
+
+					}else if(clicked_image_id == 2){
+
+						$(".pitem1").css('transform', 'scale(0)');
+						$(".pitem2").css('transform', 'scale(1)');
+						$(".pitem3").css('transform', 'scale(0)');
+						$(".pitem4").css('transform', 'scale(0)');
+						$(".unit-item img:nth-child(1)").css('transform-origin','100% 100%');
+						$(".unit-item img:nth-child(2)").css('transform-origin','0 100%');
+						$(".unit-item img:nth-child(3)").css('transform-origin','100% 0%');
+						$(".unit-item img:nth-child(4)").css('transform-origin','0 0');
+					}
+					else if(clicked_image_id == 3){
+						$(".pitem1").css('transform', 'scale(0)');
+						$(".pitem2").css('transform', 'scale(0)');
+						$(".pitem3").css('transform', 'scale(1)');
+						$(".pitem4").css('transform', 'scale(0)');
+						$(".unit-item img:nth-child(1)").css('transform-origin','100% 100%');
+						$(".unit-item img:nth-child(2)").css('transform-origin','0 100%');
+						$(".unit-item img:nth-child(3)").css('transform-origin','100% 0%');
+						$(".unit-item img:nth-child(4)").css('transform-origin','0 0');
+					}
+					else if(clicked_image_id == 4){
+						$(".pitem1").css('transform', 'scale(0)');
+						$(".pitem2").css('transform', 'scale(0)');
+						$(".pitem3").css('transform', 'scale(0)');
+						$(".pitem4").css('transform', 'scale(1)');
+						$(".unit-item img:nth-child(1)").css('transform-origin','100% 100%');
+						$(".unit-item img:nth-child(2)").css('transform-origin','0 100%');
+						$(".unit-item img:nth-child(3)").css('transform-origin','100% 0%');
+						$(".unit-item img:nth-child(4)").css('transform-origin','0 0');
+					}
+
 				}
 				else {
-					classie.addClass( el, 'active' );
+					classie.addClass( stack_grid[0], 'active' );
+					var clicked_image_id = this.id;
+					$(".pitem1").css('transform', 'scale(0.48)');
+					$(".pitem2").css('transform', 'scale(0.480)');
+					$(".pitem3").css('transform', 'scale(0.480)');
+					$(".pitem4").css('transform', 'scale(0.481)');
 				}
-			};
-
-		togglebtt.addEventListener( 'click', togglefn );
+			} );
 	} );
 })();
--- app/static/js/unit.js
+++ app/static/js/unit.js
@@ -1,15 +1,62 @@
 (function() {
-	[].slice.call( document.querySelectorAll( '.stack-grid' ) ).forEach( function( el ) {
-		var togglebtt = el.previousElementSibling,
-			togglefn = function() {
-				if( classie.hasClass( el, 'active' ) ) {
-					classie.removeClass( el, 'active' );
+	[].slice.call( document.querySelectorAll( '.unit-item' ) ).forEach( function( el ) {
+		var stack_grid = $('.stack-grid');
+		var images = [1,2,3,4];
+		el.addEventListener( 'click', function() {
+				if( classie.hasClass( stack_grid[0], 'active' ) ) {
+					classie.removeClass( stack_grid[0], 'active' );
+					var clicked_image_id = this.id;
+
+					if(clicked_image_id == 1){
+						$(".item1").css('transform', 'scale(1)');
+						$(".item2").css('transform', 'scale(0)');
+						$(".item3").css('transform', 'scale(0)');
+						$(".item4").css('transform', 'scale(0)');
+						$(".unit-item img:nth-child(1)").css('transform-origin','100% 100%');
+						$(".unit-item img:nth-child(2)").css('transform-origin','0 100%');
+						$(".unit-item img:nth-child(3)").css('transform-origin','100% 0%');
+						$(".unit-item img:nth-child(4)").css('transform-origin','0 0');
+
+					}else if(clicked_image_id == 2){
+
+						$(".item1").css('transform', 'scale(0)');
+						$(".item2").css('transform', 'scale(1)');
+						$(".item3").css('transform', 'scale(0)');
+						$(".item4").css('transform', 'scale(0)');
+						$(".unit-item img:nth-child(1)").css('transform-origin','100% 100%');
+						$(".unit-item img:nth-child(2)").css('transform-origin','0 100%');
+						$(".unit-item img:nth-child(3)").css('transform-origin','100% 0%');
+						$(".unit-item img:nth-child(4)").css('transform-origin','0 0');
+					}
+					else if(clicked_image_id == 3){
+						$(".item1").css('transform', 'scale(0)');
+						$(".item2").css('transform', 'scale(0)');
+						$(".item3").css('transform', 'scale(1)');
+						$(".item4").css('transform', 'scale(0)');
+						$(".unit-item img:nth-child(1)").css('transform-origin','100% 100%');
+						$(".unit-item img:nth-child(2)").css('transform-origin','0 100%');
+						$(".unit-item img:nth-child(3)").css('transform-origin','100% 0%');
+						$(".unit-item img:nth-child(4)").css('transform-origin','0 0');
+					}
+					else if(clicked_image_id == 4){
+						$(".item1").css('transform', 'scale(0)');
+						$(".item2").css('transform', 'scale(0)');
+						$(".item3").css('transform', 'scale(0)');
+						$(".item4").css('transform', 'scale(1)');
+						$(".unit-item img:nth-child(1)").css('transform-origin','100% 100%');
+						$(".unit-item img:nth-child(2)").css('transform-origin','0 100%');
+						$(".unit-item img:nth-child(3)").css('transform-origin','100% 0%');
+						$(".unit-item img:nth-child(4)").css('transform-origin','0 0');
+					}
+
 				}
 				else {
-					classie.addClass( el, 'active' );
+					classie.addClass( stack_grid[0], 'active' );
+					$(".item1").css('transform', 'scale(0.48)');
+					$(".item2").css('transform', 'scale(0.480)');
+					$(".item3").css('transform', 'scale(0.480)');
+					$(".item4").css('transform', 'scale(0.481)');
 				}
-			};
-
-		togglebtt.addEventListener( 'click', togglefn );
+			} );
 	} );
 })();
