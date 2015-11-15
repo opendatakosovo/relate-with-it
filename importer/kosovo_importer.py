4a8fd41c26b782c97c5f1d02f96e5d9abf8aa92c
Partin <partin.imeri@gmail.com>
2015-11-15 16:40:38 +0100

    Implemented Kosovo Budget Expenditure data importer.

--- importer/kosovo_importer.py
+++ importer/kosovo_importer.py
@@ -1,4 +1,5 @@
 from abstract_importer import AbstractImporter
+from slugify import slugify
 
 class KosovoImporter(AbstractImporter):
 
@@ -6,7 +7,7 @@ class KosovoImporter(AbstractImporter):
         pass
 
     def get_csv_filename(self):
-        return "data/kosovo/xxx.csv"
+        return "importer/data/kosovo/kosovo-budget-expenditures-2014.csv"
 
     def get_region(self):
         return 'Kosovo'
@@ -14,5 +15,37 @@ class KosovoImporter(AbstractImporter):
     def get_dataset(self):
         return 'Budget Expenditure (2014)'
 
-    def build_docs(row, self):
-        pass
+    def build_docs(self, row):
+
+
+        # In this case, it's because in the CSV doc there is a column for each year...
+        year = row[3]
+
+        # Clean expense string so that is is numerical (e.g. turn blank string to 0).
+        cost = row[2].replace(',', '')
+        if not cost.strip():
+            cost = 0
+
+        # Create doc.
+        doc = {
+            'region': {
+                'name': self.get_region(),
+                'slug': slugify(self.get_region(), to_lower=True)
+            },
+            'dataset': {
+                'name': self.get_dataset(),
+                'slug': slugify(self.get_dataset(), to_lower=True)
+            },
+            'activity': {
+                'type': row[0],
+                'description': row[1]
+            },
+            'cost': float(cost),
+            'year': int(year)
+        }
+
+
+        # Console output to provide user with feedback on status of importing process.
+        print '%s - %s: %s (%s %i)' % (doc['activity']['type'], doc['activity']['description'], doc['cost'], doc['region']['name'], doc['year'])
+
+        return [doc]
